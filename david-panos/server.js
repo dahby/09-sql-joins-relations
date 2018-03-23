078b5a7179900039bbec9ee08156bf3b6733e0a3
dahby <dstoll243@gmail.com>
2018-03-23 10:17:38 -0700

    finished sql query functions for post

--- david-panos/server.js
+++ david-panos/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = '';
+const conString = 'postgres://postgres:ibanez84@localhost:5432/postgres';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
@@ -25,7 +25,7 @@ app.get('/new', (request, response) => {
 
 // REVIEW: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
-  client.query(``)
+  client.query(`SELECT * FROM authors INNER JOIN articles ON authors.author_id=articles.author_id;`)
     .then(result => {
       response.send(result.rows);
     })
@@ -36,8 +36,10 @@ app.get('/articles', (request, response) => {
 
 app.post('/articles', (request, response) => {
   client.query(
-    '',
-    [],
+    `INSERT INTO authors(author, "authorUrl") VALUES ($1, $2) ON CONFLICT DO NOTHING;`,
+    [request.body.author,
+      request.body.authorUrl
+    ],
     function(err) {
       if (err) console.error(err);
       // REVIEW: This is our second query, to be executed when this first query is complete.
@@ -47,8 +49,8 @@ app.post('/articles', (request, response) => {
 
   function queryTwo() {
     client.query(
-      ``,
-      [],
+      `SELECT author FROM authors WHERE author=$1`,
+      [request.body.author],
       function(err, result) {
         if (err) console.error(err);
 
@@ -60,8 +62,15 @@ app.post('/articles', (request, response) => {
 
   function queryThree(author_id) {
     client.query(
-      ``,
-      [],
+      `INSERT INTO articles(author_id, author, title, "authorUrl", category, "publishedOn", body) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      [author_id,
+        request.body.author,
+        request.body.title,
+        request.body.authorUrl,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+      ],
       function(err) {
         if (err) console.error(err);
         response.send('insert complete');
@@ -149,7 +158,7 @@ function loadArticles() {
             FROM authors
             WHERE author=$5;
             `,
-              [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
+            [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
             )
           })
         })
